07085ca4971c442934f19cd22ee41595034e9084
Piscki Pratama <pisckipratama@gmail.com>
2020-03-12 10:20:13 +0700

    register, login, logout, and check endpoint done

--- server/app.js
+++ server/app.js
@@ -12,12 +12,24 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors())
 
+// database config
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://pisckipy:nopassword@reviewapi-a48cn.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+  })
+  .then(() => console.log('successfully connected with mongodb.'))
+  .catch(err => console.error(err))
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json({ title: 'Express' });
+  res.json({ message: 'API its work. Happy coding!' });
 });
 
 module.exports = router;
