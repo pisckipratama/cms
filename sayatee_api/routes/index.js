d80d71ea6393df9d2fc3b84b006cd3ab2511deb4
Piscki F. Pratama <pisckipratama@gmail.com>
2020-07-03 09:42:21 +0700

    make it better

--- sayatee_api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.json({ message: 'API its work. Happy coding!' });
-});
-
-module.exports = router;
