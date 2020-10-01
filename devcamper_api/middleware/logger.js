9f59e1c711d9fa53881e32a09e8626bb248aa2b9
Piscki F. Pratama <pisckipratama@gmail.com>
2020-10-01 15:50:43 +0700

    and again to destoy

--- devcamper_api/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-  console.log(`${req.method} \t ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-};
-
-module.exports = logger;
--- sayatee_api/v2/components/products/productAPI.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: "get all products data" });
-})
-
-module.exports = router;
